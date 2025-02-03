bed8d9c9d2db05453767bbac22702a8d5cf1d65a
Muhammed AL-Sous <154754012+Muhammed-AL-Sous@users.noreply.github.com>
2025-02-03 20:54:34 +0300

    The First Phase Of The Project

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// Select Elements
+
+let themeCheckBox = document.querySelector(".sidebar-check .toggle-checkbox");
+
+// ===================================================================================== //
+
+// Switch Dark And Light Mode
+
+/*
+The matchMedia interface is used to detect the user's preference for the Dark Mode
+appearance of the system,
+update a specific Attribute on the body element 
+and store the setting in localStorage
+*/
+
+window.addEventListener("load", () => {
+  if (window.localStorage.theme) {
+    if (window.localStorage.theme === "dark") {
+      document.body.setAttribute("data-theme", "dark");
+      themeCheckBox.checked = true;
+    } else {
+      document.body.setAttribute("data-theme", "light");
+      themeCheckBox.checked = false;
+    }
+  } else {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.body.setAttribute("data-theme", "dark");
+      themeCheckBox.checked = true;
+      window.localStorage.theme = "dark";
+    } else {
+      document.body.setAttribute("data-theme", "light");
+      themeCheckBox.checked = false;
+      window.localStorage.theme = "light";
+    }
+  }
+});
+
+themeCheckBox.addEventListener("change", (e) => {
+  document.body.setAttribute("data-theme", e.target.checked ? "dark" : "light");
+  e.target.checked
+    ? (window.localStorage.theme = "dark")
+    : (window.localStorage.theme = "light");
+});
+
+// ===================================================================================== //
